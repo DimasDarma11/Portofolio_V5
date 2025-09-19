ed2253fbbc9b203caf65a5bee8043cac88a9ef78
DimasDarma11 <dimasdarma35@gmail.com>
2025-09-19 11:20:27 +0700

    Update About.jsx

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -205,7 +205,7 @@ const AboutPage = () => {
                 data-aos="fade-right"
                 data-aos-duration="1300"
               >
-                Eki Zulfar Rachman
+                Dimaz Darma Praja
               </span>
             </h2>
             
